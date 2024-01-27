a558bd8ae706e388563acd901b8e9a3724674de7
Isaias Silva <ijsilva@apoioprodesp.sp.gov.br>
2024-01-27 15:30:24 -0300

    Add project files.

---
+++ ClassTestWorflow.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace API_TechChallengeFase2
+{
+    public class ClassTestWorflow
+    {
+        public int Id { get; set; }
+
+        public int Valor { get; set; }
+    }
+}
